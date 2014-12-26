b1ba05fea836da1cbc46e3bf7b430971718ee3ac
Amanda Rosén <Amanda@Amandas-MacBook-Pro.local>
2014-12-26 10:42:09 +0100

    Initial Commit

---
+++ Lab1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lab1
+//
+//  Created by Amanda Rosén on 2014-12-26.
+//  Copyright (c) 2014 Amanda Rosén. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
